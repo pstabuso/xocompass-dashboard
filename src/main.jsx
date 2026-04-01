42ea5fc99f512fac697b38c3eb66418a526b72bc
pstabuso <pstabuso@fit.edu.ph>
2026-04-01 12:34:27 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-// 1. Import the Provider here at the absolute root
-import { DatasetFileProvider } from './context/DatasetFileContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* 2. Wrap the entire App inside the Provider */}
-    <DatasetFileProvider>
-      <App />
-    </DatasetFileProvider>
+    <App />
   </React.StrictMode>,
 )
